5ba4fc5d8d07345898b436d4abc556d4b15d5c3e
Jonathan Pillajo <pandc19@outlook.com>
2024-08-02 07:06:04 -0500

    end section 5

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  <hr />
-
-  <MyCounter :value="5" />
-  <MyCounterScript :value="5" />
-  <MyCounterScript2 :value="5" />
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScript from './components/MyCounterScript.vue';
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
+import IndecisionView from './views/IndecisionView.vue';
 </script>
